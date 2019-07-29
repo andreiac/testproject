89e7d2f199cb7dad83f06924c1116b5a21c12169
andreia <andreia@Andreias-MacBook-Pro.local>
2019-07-29 19:04:30 -0400

    it works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,7 @@
             $("#userInput").val("");
             //comment
     
-            var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + myExpression + "&safeSearch=strict&key=AIzaSyBtTKhUd5u7vSo3QD8S0k7mIFbFJGd0ooQ";
+            var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + myExpression + "&safeSearch=moderate&key=AIzaSyBtTKhUd5u7vSo3QD8S0k7mIFbFJGd0ooQ&type=video";
             
             console.log(myExpression)
     
@@ -18,16 +18,15 @@
             method: "GET"
         
         }).then(function (response)
-        {
+
+        {console.log(response);
             var results = response.items["0"].id.videoId;
 
             console.log ("this is the variable results:" + results)
-
-
-                $("#youtubeDiv").html('<iframe id="youtubeFrame" width="560" height="315" src="https://www.youtube.com/embed/' + results + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+        
             
-
-        })
+                $("#youtubeDiv").html('<iframe id="youtubeFrame" width="560" height="315" src="https://www.youtube.com/embed/' + results + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+            })
     })
     })
 
